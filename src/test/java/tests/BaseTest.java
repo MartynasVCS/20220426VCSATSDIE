68c960ffb4b860f0b386da805b1fa0e643f0b862
MartynasVCS <martynasvcs@gmail.com>
2022-05-25 09:43:36 +0300

    Add screenshot generation on test failure

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,10 @@
 package tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
 import utilities.Driver;
+import utilities.TestListener;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
     @BeforeMethod
